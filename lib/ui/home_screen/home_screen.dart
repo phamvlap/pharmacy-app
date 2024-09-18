fd6b6855a70cfaf7896d189fd6f41261e14b59a3
minhthn <chemminh2k@gmail.com>
2024-09-18 11:31:46 +0700

    fix: resolve merge conflic in /lib/ui/home_screen/home_screen.dart

--- lib/ui/home_screen/home_screen.dart
+++ lib/ui/home_screen/home_screen.dart
@@ -10,11 +10,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: MyAppBar(),
-<<<<<<< HEAD
       body: Text('Home screen'),
-=======
-      body: Text('Home page'),
->>>>>>> thien-dev
       drawer: MyAppDrawer(),
     );
   }
