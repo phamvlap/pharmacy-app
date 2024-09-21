acb5194fc36b5137cd392e9583dae84f6e58dc67
phamlap <lapb2105580@student.ctu.edu.vn>
2024-09-22 03:23:15 +0700

    feat: build basic home screen

--- lib/ui/home_screen/home_screen.dart
+++ lib/ui/home_screen/home_screen.dart
@@ -1,17 +1,67 @@
 import 'package:flutter/material.dart';
 
+import './../../utils/app_theme.dart';
 import './../layouts/app_bar.dart';
 import './../layouts/drawer.dart';
+import './home_badge_grid.dart';
+import './home_diseases_grid.dart';
+import './products_grid.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(),
-      body: Text('Home screen'),
-      drawer: MyAppDrawer(),
+    return Scaffold(
+      appBar: const MyAppBar(),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 2.0),
+        children: [
+          HomeBadgeGrid(),
+          _buildSectionTitle(
+            iconData: Icons.manage_search,
+            title: 'GỢI Ý HÔM NAY',
+          ),
+          const ProductsGrid(),
+          _buildSectionTitle(
+            iconData: Icons.add,
+            title: 'BỆNH',
+          ),
+          const HomeDiseasesGrid(),
+        ],
+      ),
+      drawer: const MyAppDrawer(),
     );
   }
+}
+
+Widget _buildSectionTitle({required IconData iconData, required String title}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+    child: Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(3.0),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: AppColors.primaryColor,
+          ),
+          child: Icon(
+            iconData,
+            color: AppColors.whiteColor,
+            size: AppFontSizes.textNormal,
+          ),
+        ),
+        const SizedBox(width: 4.0),
+        Text(
+          title,
+          style: TextStyle(
+            color: AppColors.primaryColor,
+            fontSize: AppFontSizes.textMedium,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+  );
 }
