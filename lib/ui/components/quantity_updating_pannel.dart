41257828537f6ab290f0b685dffe103526d687ff
phamlap <lapb2105580@student.ctu.edu.vn>
2024-10-01 12:33:30 +0700

    chore: refactor QuantityUpdatingPannel

--- lib/ui/components/quantity_updating_pannel.dart
+++ lib/ui/components/quantity_updating_pannel.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../../utils/utils.dart';
 
+enum BorderRadiusType { left, right, none }
+
 class QuantityUpdatingPannel extends StatelessWidget {
   final int quantity;
   final void Function()? onIncreaseQuantityPressed;
@@ -26,22 +28,17 @@ class QuantityUpdatingPannel extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          SizedBox(
-            width: 28.0,
-            height: 26.0,
-            child: Center(
-              child: IconButton(
-                padding: const EdgeInsets.all(0.0),
-                icon: const Icon(Icons.remove),
-                iconSize: 12.0,
-                onPressed: onDecreaseQuantityPressed,
-              ),
-            ),
+          QuantityUpdatingIconButton(
+            icon: Icons.remove,
+            onPressed: onDecreaseQuantityPressed!,
+            borderRadiusType: BorderRadiusType.left,
+            borderRadius: 20.0,
           ),
           Container(
             alignment: Alignment.center,
             padding: const EdgeInsets.symmetric(
-              horizontal: 8.0,
+              horizontal: 10.0,
+              vertical: 1.0,
             ),
             decoration: BoxDecoration(
               border: Border(
@@ -61,20 +58,64 @@ class QuantityUpdatingPannel extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(
-            width: 28.0,
-            height: 26.0,
-            child: Center(
-              child: IconButton(
-                padding: const EdgeInsets.all(0.0),
-                alignment: Alignment.center,
-                icon: const Icon(Icons.add),
-                iconSize: 12.0,
-                onPressed: onIncreaseQuantityPressed,
-              ),
-            ),
+          QuantityUpdatingIconButton(
+            icon: Icons.add,
+            onPressed: onIncreaseQuantityPressed!,
+            borderRadiusType: BorderRadiusType.right,
+            borderRadius: 20.0,
           ),
         ],
+      ),
+    );
+  }
+}
+
+class QuantityUpdatingIconButton extends StatelessWidget {
+  final IconData icon;
+  final void Function() onPressed;
+  final BorderRadiusType? borderRadiusType;
+  final double borderRadius;
+
+  const QuantityUpdatingIconButton({
+    super.key,
+    required this.icon,
+    required this.onPressed,
+    this.borderRadiusType = BorderRadiusType.none,
+    required this.borderRadius,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 28.0,
+      height: 26.0,
+      child: Center(
+        child: IconButton(
+          icon: Icon(icon),
+          onPressed: onPressed,
+          padding: const EdgeInsets.all(0.0),
+          alignment: Alignment.center,
+          iconSize: 12.0,
+          style: ButtonStyle(
+            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: (borderRadiusType == BorderRadiusType.left)
+                    ? BorderRadius.only(
+                        topLeft: Radius.circular(borderRadius),
+                        bottomLeft: Radius.circular(borderRadius))
+                    : (borderRadiusType == BorderRadiusType.right)
+                        ? BorderRadius.only(
+                            topRight: Radius.circular(borderRadius),
+                            bottomRight: Radius.circular(borderRadius),
+                          )
+                        : BorderRadius.zero,
+              ),
+            ),
+            backgroundColor: WidgetStateProperty.all<Color>(
+              Colors.grey[100]!,
+            ),
+          ),
+        ),
       ),
     );
   }
