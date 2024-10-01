8d5cb14006fd5022d4754d3c8b26c330ad6175c0
phamlap <lapb2105580@student.ctu.edu.vn>
2024-10-01 20:34:26 +0700

    feat: show close icon when has user input

--- lib/ui/components/search_bar.dart
+++ lib/ui/components/search_bar.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 
-import './../../utils/utils.dart';
+import 'dart:developer';
 
-class AppSearchBar extends StatelessWidget {
+import '../../utils/utils.dart';
+
+class AppSearchBar extends StatefulWidget {
   const AppSearchBar({super.key});
+
+  @override
+  State<AppSearchBar> createState() => _AppSearchBarState();
+}
+
+class _AppSearchBarState extends State<AppSearchBar> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.addListener(
+      () {
+        setState(() {});
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 44.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      height: 40.0,
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
       decoration: BoxDecoration(
         color: AppColors.whiteColor,
-        borderRadius: BorderRadius.circular(40),
+        borderRadius: BorderRadius.circular(30),
       ),
+      alignment: Alignment.center,
       child: Row(
         children: [
           Padding(
@@ -21,26 +41,63 @@ class AppSearchBar extends StatelessWidget {
             child: Icon(
               Icons.search,
               color: AppColors.greyColor,
+              size: 24.0,
             ),
           ),
           Expanded(
-            child: GestureDetector(
-              child: TextField(
-                onTapOutside: (PointerDownEvent even) {
-                  FocusManager.instance.primaryFocus?.unfocus();
-                },
-                decoration: InputDecoration(
-                  hintText: 'Tìm kiếm thuốc, dược phẩm...',
-                  hintStyle: TextStyle(
-                    color: AppColors.greyColor,
-                    fontSize: AppFontSizes.textExtraSmall,
-                    fontWeight: FontWeight.w400,
-                  ),
-                  border: InputBorder.none,
+            child: TextField(
+              controller: _searchController,
+              onChanged: (value) {
+                log(_searchController.text);
+                setState(() {});
+              },
+              onTapOutside: (PointerDownEvent even) {
+                FocusManager.instance.primaryFocus?.unfocus();
+              },
+              decoration: InputDecoration(
+                hintText: 'Tìm kiếm thuốc, dược phẩm...',
+                hintStyle: TextStyle(
+                  color: AppColors.greyColor,
+                  fontSize: AppFontSizes.textExtraSmall,
+                  fontWeight: FontWeight.w400,
                 ),
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 12.0,
+                ),
+              ),
+              style: const TextStyle(
+                color: AppColors.blackColor,
+                fontSize: AppFontSizes.textSmall,
+                fontWeight: FontWeight.w200,
               ),
             ),
           ),
+          _searchController.text.isNotEmpty
+              ? Container(
+                  height: 22.0,
+                  width: 22.0,
+                  margin: const EdgeInsets.only(right: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[500],
+                  ),
+                  child: IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {
+                      _searchController.clear();
+                      setState(
+                        () {},
+                      );
+                    },
+                    icon: const Icon(
+                      Icons.close,
+                      color: AppColors.whiteColor,
+                      size: 20.0,
+                    ),
+                  ),
+                )
+              : const SizedBox(),
         ],
       ),
     );
