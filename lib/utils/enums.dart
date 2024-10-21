24c55b29b85bc4958fdfe7a730665ba888e0eca1
minhthn <chemminh2k@gmail.com>
2024-10-21 20:27:22 +0700

    feat: add enums

---
+++ lib/utils/enums.dart
@@ -0,0 +1,5 @@
+enum ImageType { product, user }
+
+enum Gender { male, female, other }
+
+enum OrderStatus { processing, shipping, delivered, canceled }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -2,3 +2,4 @@ export './app_theme.dart';
 export './consts.dart';
 export './functional.dart';
 export './validator.dart';
+export './enums.dart';
