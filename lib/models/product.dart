ceba050f3045b24d392b59b54c6fa432f946414e
phamvlap <phamvlap302@gmail.com>
2024-10-23 04:00:51 +0700

    chore: update product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,22 +1,23 @@
+import './image_model.dart';
+
 class Product {
-  final String id;
+  final String? id;
   final String name;
-  final List<String> imageUrls;
   final String description;
-  final List<String> category; // TODO: maybe replace with enum
+  final List<String> category;
   final String brand;
   final String producer;
   final String components;
-  final String unit; // TODO: maybe replace with enum
+  final String unit;
   final String useGuide;
   final String userTarget;
   final double price;
-  final double? salesOff;
+  final double salesOff;
+  final List<ImageModel> images;
 
   Product({
-    required this.id,
+    this.id,
     required this.name,
-    required this.imageUrls,
     required this.description,
     required this.category,
     required this.brand,
@@ -26,6 +27,80 @@ class Product {
     required this.useGuide,
     required this.userTarget,
     required this.price,
-    this.salesOff,
+    this.salesOff = 0.0,
+    this.images = const [],
   });
+
+  Product copyWith({
+    String? id,
+    String? name,
+    String? description,
+    List<String>? category,
+    String? brand,
+    String? producer,
+    String? components,
+    String? unit,
+    String? useGuide,
+    String? userTarget,
+    double? price,
+    double? salesOff,
+    List<ImageModel>? images,
+  }) {
+    return Product(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      category: category ?? this.category,
+      brand: brand ?? this.brand,
+      producer: producer ?? this.producer,
+      components: components ?? this.components,
+      unit: unit ?? this.unit,
+      useGuide: useGuide ?? this.useGuide,
+      userTarget: userTarget ?? this.userTarget,
+      price: price ?? this.price,
+      salesOff: salesOff ?? this.salesOff,
+      images: images ?? this.images,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final String categoryString = category.join(';');
+
+    return {
+      'name': name,
+      'description': description,
+      'category': categoryString,
+      'brand': brand,
+      'producer': producer,
+      'components': components,
+      'unit': unit,
+      'useGuide': useGuide,
+      'userTarget': userTarget,
+      'price': price,
+      'salesOff': salesOff,
+    };
+  }
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    final List<String> categoryList = (json['category'] as String).split(';');
+
+    return Product(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      category: categoryList,
+      brand: json['brand'],
+      producer: json['producer'],
+      components: json['components'],
+      unit: json['unit'],
+      useGuide: json['useGuide'],
+      userTarget: json['userTarget'],
+      price: json['price'].toDouble(),
+      salesOff: json['salesOff'].toDouble() ?? 0.0,
+    );
+  }
+
+  List<ImageModel> get featuredImages {
+    return images.toList();
+  }
 }
