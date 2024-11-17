93fcba20b97961cc185ec3388784836f014d1916
minhthn <chemminh2k@gmail.com>
2024-11-17 20:43:47 +0700

    fix: add missing lib/controllers/order_detail_controller.dart

---
+++ lib/controllers/order_detail_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'dart:developer';
+
+import '../models/models.dart';
+import '../services/services.dart';
+import '../utils/utils.dart';
+
+class OrderDetailController with ChangeNotifier {
+  final OrderDetailService orderDetailService = OrderDetailService();
+  final List<OrderDetail> _orderDetails = [];
+
+  Future<void> fetchAllOrderDetails(OrderStatus orderStatus) async {
+    final List<OrderDetail>? orders =
+        await orderDetailService.fetchAllOrderDetails(orderStatus);
+    if (orders != null) {
+      _orderDetails.clear();
+      _orderDetails.addAll(orders);
+      notifyListeners();
+    }
+  }
+
+  List<OrderDetail> get orders {
+    return [..._orderDetails];
+  }
+}
